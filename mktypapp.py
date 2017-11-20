e8c151aa4a195bcdb50ba053496fce2a78a60a9b
brianna <bhowell2017@brandeis.edu>
2017-11-20 14:56:53 -0500

    added chat page

--- mktypapp.py
+++ mktypapp.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
-
+from datetime import datetime
 
 @app.route('/')
 def main():
@@ -46,7 +46,18 @@ def Lijep7():
 def ryan():
 	return render_template('ryan.html')
 
-
+messages=[]
+@app.route('/chat',methods=['GET','POST'])
+def chat():
+	if request.method == 'POST':
+		msg = request.form['msg']
+		who = request.form['who']
+		now = datetime.now()
+		x = {'msg':msg,'now':now,'who':who}
+		messages.insert(0,x) # add msg to the front of the list
+		return render_template("chat.html",messages=messages)
+	else:
+		return render_template("chat.html",messages=[])
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=3000)
