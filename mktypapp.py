d192b14eed66a204afdbc0edea29f9a1b0b17537
tim <tjhickey@brandeis.edu>
2017-12-06 21:06:28 -0500

    added authentication and MVP

--- mktypapp.py
+++ mktypapp.py
@@ -1,18 +1,169 @@
-from flask import Flask, render_template, request
-app = Flask(__name__)
+"""
+    Brandeis Eats
+	Developers:
+
+
+    The authentication comes from an app by Bruno Rocha
+    GitHub: https://github.com/rochacbruno
+"""
+from functools import wraps
+from flask import Flask, redirect, url_for, session, request, jsonify, render_template, request
+from flask_oauthlib.client import OAuth
 from datetime import datetime
+
+app = Flask(__name__)
+app.config['GOOGLE_ID'] = '246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com'
+app.config['GOOGLE_SECRET'] = 'iqgLqu6pXgLuHsZFq6nvxDX3'
+
+
+app.debug = True
+app.secret_key = 'development'
+oauth = OAuth(app)
+
+google = oauth.remote_app(
+    'google',
+    consumer_key=app.config.get('GOOGLE_ID'),
+    consumer_secret=app.config.get('GOOGLE_SECRET'),
+    request_token_params={
+        'scope': 'email'
+    },
+    base_url='https://www.googleapis.com/oauth2/v1/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+)
+
+def require_login(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not('google_token' in session):
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.route('/main')
+def index():
+    if 'google_token' in session:
+        me = google.get('userinfo')
+        print("logged in")
+        print(jsonify(me.data))
+        return render_template("main.html")
+        #return jsonify({"data": me.data})
+    print('redirecting')
+    return redirect(url_for('login'))
+
+
+@app.route('/login')
+def login():
+    return google.authorize(callback=url_for('authorized', _external=True))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('google_token', None)
+    #
+    return redirect(url_for('main'))
+
+
+@app.route('/login/authorized')
+def authorized():
+    resp = google.authorized_response()
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['google_token'] = (resp['access_token'], '')
+    print(session['google_token'])
+    me = google.get('userinfo')
+    session['userinfo'] = me.data
+    print(me.data)
+    return render_template("main.html")
+    #return jsonify({"data": me.data})
+
+
+@google.tokengetter
+def get_google_oauth_token():
+    return session.get('google_token')
+
+
+
 
 @app.route('/')
 def main():
 	return render_template("main.html")
 
 @app.route('/buy')
+@require_login
 def buy():
 	return render_template('buy.html')
 
 @app.route('/sell')
+@require_login
 def sell():
 	return render_template('sell.html')
+
+books=[]
+bookCounter=0
+
+@app.route('/processSell',methods=['GET','POST'])
+@require_login
+def processSell():
+	global books
+	global bookCounter
+	if request.method == 'POST':
+		userinfo = session['userinfo']
+		who = userinfo['email']
+		t = request.form['title']
+		cou = request.form['course']
+		l = request.form['link']
+		con = request.form['condition']
+		p = request.form['price']
+		d = request.form['description']
+		n = datetime.now()
+
+		book = {
+            'id':bookCounter,
+            'title':t,
+            'course':cou,
+            'link':l,
+            'condition':con,
+            'price':p,
+            'description':d,
+			'when':n,
+			'who':who
+            }
+		bookCounter = bookCounter + 1
+		books.insert(0,book) # add msg to the front of the list
+
+	return render_template("BooksForSale.html",books=books)
+
+
+
+@app.route('/booksForSale')
+def booksForSale():
+	global books
+	print("in booksForSale")
+	print(books)
+	return render_template('booksForSale.html',books=books)
+
+
+@app.route('/removeBook',methods=['GET','POST'])
+def takeOrder():
+	global books
+	if request.method == 'POST':
+		userinfo = session['userinfo']
+		who = userinfo['email']
+		seller = request.form['who']
+		num = request.form['id']
+		newBooks = [x for x in books if not(x['who']==who and int(x['id'])==int(num)) ]
+		print("newBooks=")
+		print(newBooks)
+		books = newBooks
+	return render_template("booksForSale.html",books=books)
+
+
 
 @app.route('/pitch')
 def pitch():
@@ -25,6 +176,7 @@ def formdemo():
 
 
 @app.route('/rent')
+@require_login
 def rent():
 	return render_template('rent.html')
 
@@ -73,4 +225,4 @@ def chat():
 		return render_template("chat.html",messages=[])
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=3000)
+    app.run('0.0.0.0',port=5000)
